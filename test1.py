6ee9efd90539277b7633625ee141acb34369ffca
VelotianaR <158043591+VelotianaR@users.noreply.github.com>
2024-03-07 12:19:43 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,3 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+
 progress_bar = st.sidebar.progress(0)
 status_text = st.sidebar.empty()
 last_rows = np.random.randn(1, 1)
